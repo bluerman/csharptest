4a9ed7783c3e0cbe578d008879108e5a5e8a06d5
Brian Luerman <brian.luerman@gmail.com>
2022-02-03 14:59:47 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TestApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Tell me your name");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
